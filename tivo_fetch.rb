3ec4bd41852178b3aea3d5cf7765587225da3fd4
Ian Burrell <ianburrell@gmail.com>
2009-01-10 19:32:11 -0800

    add skip option; add numbers; add sleep

--- tivo_fetch.rb
+++ tivo_fetch.rb
@@ -6,23 +6,41 @@ require 'votigoto'
 require 'duration'
 require 'tempfile'
 
-def fetch(tivo, program_id) 
+def make_filename(show_name, extension, options)
+    filename = show_name + '.' + extension
+    if options[:skip] then
+        if File.exists?(filename) && File.size(filename) > 100 then
+            puts "skipping #{filename}"
+            return nil
+        end
+    else 
+        counter = 0
+        while File.exists?(filename) && File.size(filename) > 100 do
+            counter += 1
+            filename = (show_name + '.' + counter.to_s + '.' + extension)
+        end
+    end
+    puts "fetch #{filename}"
+    return filename
+end
+
+def fetch(tivo, program_id, options) 
     show = tivo.show(program_id)
     url = show.content_url
     mak = tivo.mak
-    filename = show.to_s + '.tivo'
+    filename = make_filename(show.to_s, 'tivo', options)
     cookie_file = Tempfile.new('cookies').path
-    puts filename
+    return unless filename
     system(%Q{curl --cookie-jar #{cookie_file} --digest --user "tivo:#{mak}" "#{url}" -o "#{filename}"})
 end
 
-def fetch_and_decode(tivo, program_id) 
+def fetch_and_decode(tivo, program_id, options) 
     show = tivo.show(program_id)
     url = show.content_url
     mak = tivo.mak
-    filename = show.to_s + '.mpg'
+    filename = make_filename(show.to_s, 'mpg', options)
     cookie_file = Tempfile.new('cookies').path
-    puts filename
+    return unless filename
     system(%Q{curl --cookie-jar #{cookie_file} --digest --user "tivo:#{mak}" "#{url}" | tivodecode --mak #{mak} -o "#{filename}" -})
 end
 
@@ -43,10 +61,10 @@ def print_details(show)
     puts "source_station: #{show.source_station}"
     puts "program_id: #{show.program_id}"
     puts "series_id: #{show.series_id}"
-    puts "source_size: " + nice_file_size(show.source_size)
-    puts "duration: " + nice_duration(show.duration)
+    puts "source_size: #{nice_file_size(show.source_size)})"
+    puts "duration: #{nice_duration(show.duration)}"
     puts "source_channel: #{show.source_channel}"
-    puts "capture_date: " + show.capture_date.strftime("%Y-%m-%d %H:%M:%S")
+    puts "capture_date: #{show.capture_date.strftime("%Y-%m-%d %H:%M:%S")}"
     puts "in_progress: #{show.in_progress}"
     puts
 end
@@ -70,6 +88,9 @@ OptionParser.new do |opts|
     opts.on("--decode", "Decode") { |bool|
         options[:decode] = bool
     }
+    opts.on("--skip", "Skip existing files") { |bool|
+        options[:skip] = bool
+    }
 end.parse!
 
 tivo = Votigoto::Base.new(host, mak)
@@ -79,9 +100,11 @@ if ! ARGV.empty? then
         if options[:detail] then
             print_details(tivo.show(id))
         elsif options[:decode] then
-            fetch_and_decode(tivo, id)
+            fetch_and_decode(tivo, id, options)
+            sleep(5)
         else
-            fetch(tivo, id)
+            fetch(tivo, id, options)
+            sleep(5)
         end
     }
 else
